e3e97cb71cf32aa5caa81499099d39287395ae49
FirstBench Academy <firstbenchacademy1@gmail.com>
2022-09-07 17:18:30 -0400

    Updated

--- intro_feetwet/j1.js
+++ intro_feetwet/j1.js
@@ -1,4 +1,18 @@
-function hello(){
-	console.log("Hello World 3!!");
-	console.log("Hello World 4 !!");
+
+let add = function(a , b){
+	return a + b;
 }
+
+let minus = (a , b) => {
+	return a - b;
+}
+
+function hello(){
+console.log('ADDed :: ',add(1,3))
+console.log('Subtracted  :: ',minus(1,3))
+
+}
+
+// function add(a, b){
+// 	return a + b;
+// }
